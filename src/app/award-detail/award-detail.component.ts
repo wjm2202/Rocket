8952997fab141cf36c7790cdafc555e1a2947bca
Glen Osborne <em9777@aut.ac.nz>
2017-08-22 17:48:49 +1200

    http get for single award for award detail page

--- src/app/award-detail/award-detail.component.ts
+++ src/app/award-detail/award-detail.component.ts
@@ -4,36 +4,48 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { GetawardsService } from '../services/getawards.service';
 import { AwardModel } from '../interfaces/awardcard';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-award-detail',
   templateUrl: './award-detail.component.html',
   styleUrls: ['./award-detail.component.css'],
-  providers: [GetawardsService]
+  providers: []
 })
 export class AwardDetailComponent implements OnInit {
-  award:AwardModel;
-  constructor(private awardsSearchSearch: GetawardsService,
+  obj;
+  AwardName;
+  StudentName;
+  AwardDegreeLevel;
+  AwardDegree;
+  AwardDate;
+  AwardBlurb;
+  constructor(
               private route: ActivatedRoute,
               private location: Location,
-              private http: Http) { }
+              private http: Http) { 
+                this.getSingleAward();
+              }
 
   getSingleAward(){
-    //this.http.get('https://webservices-test.aut.ac.nz/ecms/api/awards/'+id)
+    this.http.get('https://webservices-test.aut.ac.nz/ecms/api/awards/'+this.route.snapshot.params['id'])
+    .subscribe(
+      (res: Response) => {
+        this.obj = res.json();
+        console.log(this.obj);
+        this.AwardName = this.obj.AwardName;
+        this.StudentName = this.obj.StudentName;
+        this.AwardDegreeLevel = this.obj.AwardDegreeLevel;
+        this.AwardDegree = this.obj.AwardDegree;
+        this.AwardDate = this.obj.AwardDate;
+        this.AwardBlurb = this.AwardBlurb;
+      }
+    )
   }
 
   ngOnInit() {
-    this.getAwards();
-    console.log('values of award model '+this.award.awardDegree);
   }
   back(){
     this.location.back();
-  }
-  getAwards(): void {
-    this.awardsSearchSearch.getAward(+this.route.snapshot.params['id'])
-    .subscribe(resultArray => this.award = resultArray,
-    error => console.log("Error :: "+ error)
-   )
   }
 }
