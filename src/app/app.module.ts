61a11b3a138ad7edf576eeb1cf3ad2f151153ab2
Glen Osborne <em9777@aut.ac.nz>
2017-08-24 08:08:39 +1200

    update libaries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -49,11 +49,7 @@ import { BecomesponsorComponent } from './becomesponsor/becomesponsor.component'
 
 import { SearchComponent } from './search/search.component';
 import { AwardDetailComponent } from './award-detail/award-detail.component';
-<<<<<<< HEAD
 import { SponsorCaraComponent } from './sponsor-cara/sponsor-cara.component';
-
-=======
->>>>>>> upstream/master
 import { SignsponsorComponent } from './signsponsor/signsponsor.component';
 //material design module
 import { MaterialModule } from '@angular/material';
