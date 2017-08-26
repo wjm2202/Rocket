bfbfd800695faf55fe0e2c131b9981467ac7f001
wjm2202 <wjm2202@aut.ac.nz>
2017-08-26 13:34:42 +1200

    move contact details to footer and change to on click

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,9 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  open:boolean = false;
   constructor() { }
-
+  show(){
+    this.open = !this.open;
+  }
   ngOnInit() {
   }
 
