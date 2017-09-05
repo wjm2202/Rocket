0a07e0b9a5f50a6f92c5c84298c166e607f3565d
Jayce-C <Blackswankk@gmail.com>
2017-09-06 10:50:39 +1200

    Sponsorship Message

--- src/app/sponsorship-message/sponsorship-message.component.ts
+++ src/app/sponsorship-message/sponsorship-message.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+interface SponsorshipModel {
+  Message: string;
+  isVisable: boolean;
+}
 
 @Component({
   selector: 'app-sponsorship-message',
@@ -6,10 +12,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sponsorship-message.component.css']
 })
 export class SponsorshipMessageComponent implements OnInit {
-
-  constructor() { }
+  messages: SponsorshipModel;
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
+    this.getMessage();
   }
-
+  getMessage() {
+    this.httpClient.get<SponsorshipModel>('https://webservices-test.aut.ac.nz/ecms/api/sponsorship').subscribe
+      (
+      data => {
+        this.messages = data;
+      },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log('Client-side Error Occured');
+        } else {
+          console.log('Server-side Error Occured');
+        }
+      }
+      )
+  }
 }
