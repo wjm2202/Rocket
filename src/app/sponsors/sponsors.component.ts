5bc08582c10fc4958d512c59b10504f9f9d63474
Glen Osborne <em9777@aut.ac.nz>
2017-09-14 14:45:10 +1200

    complete auto year selection

--- src/app/sponsors/sponsors.component.ts
+++ src/app/sponsors/sponsors.component.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpErrorResponse} from '@angular/common/http';
 import { UrlStrip } from '../pipes/imagestrip';
 import { PhotoPipe } from '../pipes/photo.pipe';
 import { GetMainSponsorService } from '../services/getSponsor.service';
+import { Observable } from 'rxjs/Observable';
 
 interface SponsorsModel {
   SponsorBlurb:string;
@@ -21,10 +22,30 @@ interface SponsorsModel {
 })
 export class SponsorsComponent implements OnInit {
   sponsors:SponsorsModel[];
-  constructor(private getSponsors:GetMainSponsorService) { }
+  temp:SponsorsModel[];
+  complete:boolean = false;
+  constructor(private getSponsors:GetMainSponsorService) { 
+
+  }
+  shufflearray<T>(array: T[]): T[]{
+    if(array.length <= 1){
+      return array;
+    }
+    for (let i =0;i<array.length;i++){
+      const randomChoiceIndex = Math.random() * ((array.length -1)-1) + 1;
+      [array[i], array[randomChoiceIndex]] = [array[randomChoiceIndex], array[i]];
+    }
+    array.forEach(element => {
+      console.log(element);
+    });
+    return array;
+  }
+
    getSponsor(): void {
     this.getSponsors.getAllSponsorPics()
-    .subscribe(resultArray => this.sponsors = resultArray,
+    .subscribe(resultArray => {
+      this.sponsors = resultArray;
+    },
     error => console.log("Error :: "+ error)
    )
   }
